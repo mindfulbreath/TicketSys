7739d866a39237bcd7371dd6083d085eed7c9d9d
Joan Ging <mindfulbreath@gmail.com>
2011-10-24 23:11:17 -0700

    modified temporary message

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TicketSys</title>
+	<style type="text/css">
+		#notice {
+			-webkit-border-radius: 5px;
+			-moz-border-radius: 5px;
+			border-radius: 5px;
+			margin: 10px;
+			padding: 5px;
+			background: #396;
+			border-top: 1px solid #6c9;
+			border-left: 1px solid #6c9;
+			border-right: 1px solid #063;
+			border-bottom: 1px solid #063;
+			text-shadow: 1px 1px 1px #063;
+			font-family: Arial, sans-serif;
+			font-weight: bold;
+			font-size: 0.8em;
+			color: #fff;
+			text-align: center;
+		}
+	</style>
+</head>
+
+<body>
+	<div id="notice">This deployment is working properly.</div>
+</body>
+
+</html>
